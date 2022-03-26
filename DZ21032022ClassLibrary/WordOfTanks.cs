66492bedae584b8186c097fdf9daf7e57b5ae83d
Rhoxolan <pba17mail@gmail.com>
2022-03-27 01:37:50 +0200

    Add

---
+++ DZ21032022ClassLibrary/WordOfTanks.cs
@@ -0,0 +1,58 @@
+﻿namespace WordOfTanks
+{
+    public class Tank
+    {
+        public string Model { get; set; }
+        public short Ammunition { get; set; }
+        public short Armor { get; set; }
+        public short Mobility { get; set; }
+
+        public Tank(string model, short ammunition, short armor, short mobility)
+        {
+            Model = model;
+            Ammunition = ammunition;
+            Armor = armor;
+            Mobility = mobility;
+        }
+
+        public static bool operator > (Tank tank1, Tank tank2)
+        {
+            int tank1_points = 0;
+            int tank2_points = 0;
+            tank1_points = (tank1.Ammunition > tank2.Ammunition) ? tank1_points++ : tank2_points++;
+            tank1_points = (tank1.Armor > tank2.Armor) ? tank1_points++ : tank2_points++;
+            tank1_points = (tank1.Mobility > tank2.Mobility) ? tank1_points++ : tank2_points++;
+            if (tank1_points == tank2_points)
+            {
+                return true && ((tank1.Armor+tank1.Ammunition+tank1.Mobility) >
+                    (tank2.Armor + tank2.Ammunition + tank1.Mobility));
+            }
+            if (tank1_points > tank2_points)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static bool operator <(Tank tank1, Tank tank2)
+        {
+            return !(tank1 > tank2);
+        }
+
+        public static Tank operator* (Tank tank1, Tank tank2)
+        {
+            if(tank1 > tank2)
+            {
+                return tank1;
+            }
+            else
+            {
+                return tank2;
+            }
+        }
+    }
+}
