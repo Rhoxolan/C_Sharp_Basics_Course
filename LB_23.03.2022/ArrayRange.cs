b38149d0388f835074bd01cd6a272f22c6cb5135
Rhoxolan <pba17mail@gmail.com>
2022-03-27 22:14:25 +0300

    Add

---
+++ LB_23.03.2022/ArrayRange.cs
@@ -0,0 +1,68 @@
+﻿namespace program
+{
+    using System.Collections;
+
+    public class ArrayRange<T> : IEnumerable
+    {
+        int begin;
+        int end;
+        uint capacity;
+        T[] array;
+
+        public ArrayRange(int begin, int end)
+        {
+            if (begin > end)
+            {
+                int buf = begin;
+                begin = end;
+                end = buf;
+            }
+            this.begin = begin;
+            this.end = end;
+            capacity = (uint)(end - begin) + 1;
+            array = new T[capacity];
+        }
+        public IEnumerator GetEnumerator() => array.GetEnumerator();
+
+        public uint Capacity
+        {
+            get { return capacity; }
+        }
+
+        public int Begin
+        {
+            get { return begin; }
+        }
+
+        public int End
+        {
+            get { return end; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < begin && index > end)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    return array[index - begin];
+                }
+            }
+            set
+            {
+                if (index < begin && index > end)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    array[index - begin] = value;
+                }
+            }
+        }
+    }
+}
---
+++ LB_23.03.2022/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace program
+{
+    using System;
+    using static MyClassLibrary.MyConsoleFunctional;
+
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Пожалуйста, выбирите задание (1 или 2)\n0 - Выход\n\n");
+                choice = NumberInput(0, 2);
+                Console.Clear();
+                if (choice > 1)
+                {
+                    Task2();
+                }
+                else if (choice > 0)
+                {
+                    Task1();
+                }
+            } while (choice != 0);
+        }
+
+        static void Task1()
+        {
+            Console.WriteLine("Пожалуйста, введите начало диапазона: ");
+            int begin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Пожалуйста, введите конец диапазона: ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            ArrayRange<int> ar = new(begin, end);
+            for (int i = begin; i <= end; i++)
+            {
+                Console.WriteLine($"Пожалуйста, введите элемент {i}: ");
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Наш массив на {ar.Capacity} элементов c индексами от " +
+                $"{ar.Begin} до {ar.End}: ");
+            foreach (var i in ar)
+            {
+                Console.Write(i + " ");
+            }
+            AnyKey();
+        }
+
+        static void Task2()
+        {
+            AnyKey();
+        }
+    }
+}
