7f841ae8f0c979ab2006924b529fba874e9cdc93
Rhoxolan <pba17mail@gmail.com>
2022-04-04 15:46:12 +0300

    Add LB

---
+++ LB_30.03.2022/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace program
+{
+    interface IHawk
+    {
+        bool Attack
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+
+    interface IPray
+    {
+        bool Run
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+
+    internal class Tiger : IHawk
+    {
+        string AnimalName { get; set; }
+
+        public Tiger(string animalName)
+        {
+            AnimalName = animalName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Тигр";
+            }
+        }
+    }
+
+    internal class Capreolus : IPray
+    {
+        string AnimalName { get; set; }
+
+        public Capreolus(string animalName)
+        {
+            AnimalName = animalName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Косуля";
+            }
+        }
+    }
+
+    internal class Dog : IHawk, IPray
+    {
+        string AnimalName { get; set; }
+
+        public Dog(string animalName)
+        {
+            AnimalName = animalName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Собачка";
+            }
+        }
+    }
+}
---
+++ LB_30.03.2022/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static MyClassLibrary.MyConsoleFunctional;
+using MyClassLibrary;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Пожалуйста, выбирите задание (1 - 2)\n0 - Выход\n\n");
+                choice = NumberInput(0, 2);
+                Console.Clear();
+                if (choice > 1)
+                {
+                    Task2();
+                }
+                else if (choice > 0)
+                {
+                    Task1();
+                }
+            } while (choice != 0);
+        }
+
+        static void Task1()
+        {
+            IHawk tiger = new Tiger("Вася");
+            IPray capreolus = new Capreolus("Бэмби");
+            IHawk dog = new 
+            AnyKey();
+        }
+
+        static void Task2()
+        {
+            AnyKey();
+        }
+    }
+}
