d4be1c9472757e3660be14df2a2a2eb4486fbd9e
Rhoxolan <pba17mail@gmail.com>
2022-04-06 20:04:32 +0300

    Add to LB

---
+++ LB_01.04.2022/HeightDeterminant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using static MyClassLibrary.MyConsoleFunctional;
+
+namespace program
+{
+    internal class HeightDeterminant
+    {
+        private delegate void ShowHeight(string message, int val);
+        private delegate void ShowMessage(string message);
+        private static ShowHeight showHeight = HeightMessage;
+        private static ShowMessage showMessage = Message;
+
+        private static void HeightMessage(string message, int height)
+        {
+            string wait = "Рассчитываем ваш рост";
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != 0)
+                    Console.WriteLine(wait += ".");
+                else
+                    Console.WriteLine(wait);
+                Thread.Sleep(700);
+            }
+            Console.WriteLine();
+            Console.WriteLine(message + height + " см!");
+        }
+
+        private static void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Run()
+        {
+            showMessage("Добро пожаловать в программу-определитель вашего роста!\n" +
+                "Пожалуйста, введите ваш рост: ");
+            int height = NumberInput(0, 300);
+            showHeight("Ваш Рост: ", height);
+            showMessage("\nСпасибо за ипользование программы определителя-роста!");
+        }
+    }
+}
--- LB_01.04.2022/Program.cs
+++ LB_01.04.2022/Program.cs
@@ -4,6 +4,8 @@ using MyClassLibrary;
 
 namespace program
 {
+    delegate int Operation(int a, int b);
+
     class Program
     {
         static void Main()
@@ -40,11 +42,38 @@ namespace program
 
         static void Task1()
         {
+            HeightDeterminant.Run();
             AnyKey();
         }
 
         static void Task2()
         {
+            Operation? op = null;
+            if (op is null)
+            {
+                Console.WriteLine("op is null");
+            }
+            else
+            {
+                Console.WriteLine($"Сложение {1} и {3}: {op(1, 3)}");
+            }
+            op = Add;
+            if (op is null)
+            {
+                Console.WriteLine("op is null");
+            }
+            else
+            {
+                Console.WriteLine($"Сложение {1} и {3}: {op(1, 3)}");
+            }
+            op = null;
+            int? res = op?.Invoke(1, 3);
+            Console.WriteLine($"Умножение {1} и {3}: ");
+            Console.WriteLine(res ?? null);
+            op = Mult;
+            res = op?.Invoke(1, 3);
+            Console.WriteLine($"Умножение {1} и {3}: ");
+            Console.WriteLine(res ?? null);
             AnyKey();
         }
 
@@ -56,6 +85,21 @@ namespace program
         static void Task4()
         {
             AnyKey();
+        }
+
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        private static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        private static int Mult(int a, int b)
+        {
+            return a * b;
         }
     }
 }
