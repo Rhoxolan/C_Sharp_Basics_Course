e0ca0939b2fa800e7ea11c599770c003748e09d3
Rhoxolan <pba17mail@gmail.com>
2022-04-06 15:18:28 +0300

    Add LB 01.04.2022

---
+++ LB_01.04.2022/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static MyClassLibrary.MyConsoleFunctional;
+using MyClassLibrary;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Пожалуйста, выбирите задание (1 - 4)\n0 - Выход\n\n");
+                choice = NumberInput(0, 2);
+                Console.Clear();
+                if (choice > 4)
+                {
+                    Task4();
+                }
+                else if (choice > 3)
+                {
+                    Task3();
+                }
+                else if (choice > 1)
+                {
+                    Task2();
+                }
+                else if (choice > 0)
+                {
+                    Task1();
+                }
+            } while (choice != 0);
+        }
+
+        static void Task1()
+        {
+            AnyKey();
+        }
+
+        static void Task2()
+        {
+            AnyKey();
+        }
+
+        static void Task3()
+        {
+            AnyKey();
+        }
+
+        static void Task4()
+        {
+            AnyKey();
+        }
+    }
+}
