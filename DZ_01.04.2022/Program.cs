8d180a4cb641d00846945b07fef612b97dd30c54
Rhoxolan <pba17mail@gmail.com>
2022-04-07 23:50:44 +0300

    Add to DZ

--- DZ_01.04.2022/Program.cs
+++ DZ_01.04.2022/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static MyClassLibrary.MyConsoleFunctional;
+using MyClassLibrary;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Пожалуйста, выбирите задание (1 - 3)\n0 - Выход\n\n");
+                choice = NumberInput(0, 3);
+                Console.Clear();
+                if (choice > 2)
+                {
+                    Task3();
+                }
+                else if (choice > 1)
+                {
+                    Task2();
+                }
+                else if (choice > 0)
+                {
+                    Task1();
+                }
+            } while (choice != 0);
+        }
+
+        static void Task1()
+        {
+            AnyKey();
+        }
+
+        static void Task2()
+        {
+            AnyKey();
+        }
+
+        static void Task3()
+        {
+            AnyKey();
+        }
+    }
+}
