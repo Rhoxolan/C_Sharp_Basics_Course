74dbf6e29f5e00551f984b804b0d32a6568473dc
Rhoxolan <pba17mail@gmail.com>
2022-04-11 11:21:46 +0300

    Add DZ

---
+++ DZ_04.04.2022/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using static MyClassLibrary.MyConsoleFunctional;
+using MyClassLibrary;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Пожалуйста, выбирите задание (1 - 6)\n0 - Выход\n\n");
+                choice = NumberInput(0, 6);
+                Console.Clear();
+                if (choice > 5)
+                {
+                    Task6();
+                }
+                else if (choice > 4)
+                {
+                    Task5();
+                }
+                else if (choice > 3)
+                {
+                    Task4();
+                }
+                else if (choice > 2)
+                {
+                    Task3();
+                }
+                else if (choice > 1)
+                {
+                    Task2();
+                }
+                else if (choice > 0)
+                {
+                    Task1();
+                }
+            } while (choice != 0);
+        }
+
+        static void Task1()
+        {
+            AnyKey();
+        }
+
+        static void Task2()
+        {
+            AnyKey();
+        }
+
+        static void Task3()
+        {
+            AnyKey();
+        }
+
+        static void Task4()
+        {
+            AnyKey();
+        }
+
+        static void Task5()
+        {
+            AnyKey();
+        }
+
+        static void Task6()
+        {
+            AnyKey();
+        }
+    }
+}
