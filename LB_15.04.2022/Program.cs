952b4441b22688d3aa1ef894fa2358b4c682e5a4
Rhoxolan <pba17mail@gmail.com>
2022-04-19 21:24:47 +0300

    Add LB

---
+++ LB_15.04.2022/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static MyClassLibrary.MyConsoleFunctional;
+using MyClassLibrary;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Определение пути к папке документы
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            //Запись в файл текста с помощью класса File
+            File.WriteAllText(Path.Combine(docPath, "File.txt"), "Строка");
+            //Добавление текста в существующий файл без перезаписи с помощью класса File
+            File.AppendAllText(Path.Combine(docPath, "File.txt"), "\nСтрока1\n");
+            //Добавление целочисленного значения в существующий файл без перезаписи с помощью класса File
+            File.AppendAllText(Path.Combine(docPath, "File.txt"), 1.ToString());
+
+            //Считывание текста c файла с помощью класса File
+            Console.WriteLine(File.ReadAllText(Path.Combine(docPath, "File.txt")));
+
+            //Создаем, считываем и записываем ASCII фаил с помощью класса File
+            File.WriteAllText(Path.Combine(docPath, "AsciiFile.txt"), "This is Ascii", Encoding.ASCII);
+            Console.WriteLine(File.ReadAllText(Path.Combine(docPath, "AsciiFile.txt"), Encoding.ASCII));
+        }
+    }
+}
