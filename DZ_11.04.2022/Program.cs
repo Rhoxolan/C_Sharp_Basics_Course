f99bfb93b443a8fc3636558c4655b2616cd3a25c
Rhoxolan <pba17mail@gmail.com>
2022-04-22 16:09:36 +0300

    Add to DZ

--- DZ_11.04.2022/Program.cs
+++ DZ_11.04.2022/Program.cs
@@ -9,8 +9,67 @@ namespace program
     {
         static void Main()
         {
-            Game game = new Game();
-            game.ToGame();
+            //Задание "Карточная игра"
+            {
+                Game game = new Game();
+                Console.WriteLine("Игра: ");
+                game.ToGame();
+                Console.WriteLine("\n\n");
+            }
+            //Проработка функционала коллекций
+            {
+                //List<T>
+                var values = new List<MyRecord>() { new(1), new(10) };
+                var values2 = new List<MyRecord>(values) { new(50) }; //Инициализируем values2 значениями values,
+                                                                      //добавляя 1 элемент
+                foreach (var v in values2)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+
+
+                var values3 = values2; //Присвоение ссылки с последующей демонстрацией изменений по ссылке
+                foreach (var v in values3)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+                values2[1].Value = 300;
+                foreach (var v in values3)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+
+                List<int> l = new(100); //Указываем capacity списка
+                Console.WriteLine(l.Capacity);
+
+                values3.Reverse(); //Переворачиваем спиком
+                foreach (var v in values3)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+
+                values3.RemoveAt(2); //Удаляем элементы списка 2-мя способами
+                values3.Remove(new(300));
+                foreach (var v in values3)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+
+                List<MyRecord> values4 = new() { new(10), new(100) };
+                List<MyRecord> values5 = new() { new(30), new(300) };
+                values4.AddRange(values5); //Добавляем коллекцию к коллекции
+                values4[3] = new(70);
+                foreach (var v in values4)
+                {
+                    Console.Write(v + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 
@@ -20,8 +79,11 @@ namespace program
 
         public Game()
         {
-            players = new List<Player>() { new(new()), new(new()) };
             Random random = new Random();
+
+            players = new List<Player>() { new(new()), new(new()) }; //Создаём двух игроков
+
+            //Тасуем карты
             for (int i = 0; i < 16; i++)
             {
                 players[0].Cards.Add(new(random.Next(1, 5), random.Next(6, 14)));
@@ -37,7 +99,7 @@ namespace program
             List<int> counts = new() { 0, 0 };
             for (int i = 0; i < 16; i++)
             {
-                if(players[0].Cards[i].Type > players[1].Cards[i].Type)
+                if (players[0].Cards[i].Type > players[1].Cards[i].Type)
                     counts[0]++;
                 else
                     counts[1]++;
@@ -46,11 +108,25 @@ namespace program
                 Console.WriteLine($"Победил Игорк 1");
             else
                 Console.WriteLine($"Победил Игорк 2");
-            AnyKey();
         }
     }
 
-    record Player (List<Card> Cards);
+    record Player(List<Card> Cards);
 
-    record Card (int Suit, int Type);
+    record Card(int Suit, int Type);
+
+    record MyRecord
+    {
+        public MyRecord(int val)
+        {
+            Value = val;
+        }
+
+        public int Value { get; set; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
 }
